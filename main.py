af3ffcd6864941290c4d568974adeed5731a1a8c
Brian Klug <brian.klug@gmail.com>
2013-07-24 19:32:11 -0700

    Working model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util, template
+from google.appengine.ext import db
 import logging
 import pprint
 import urllib
@@ -12,62 +13,35 @@ import re
 import json
 
 
+
+class AlarmEvent(db.Model):
+  event = db.StringProperty(required=True)
+  zone = db.StringProperty(required=True)
+  created = db.DateTimeProperty(auto_now_add=True)
+
 class IndexHandler(webapp.RequestHandler):
     def get(self):
+        alarmevents = AlarmEvent.all().fetch(100)
         self.response.out.write(template.render('templates/index.html', locals()))
 
-#class LogHandler(webapp.RequestHandler):
-#    def get(self, pagename, site = PB_WIKI):
-#        skip_cache = self.request.get('cache') == '0'
-#        version = os.environ['CURRENT_VERSION_ID']
-#        shouldRedirect = False
-#
-#        redirect_urls = {
-#          # From: To
-#          'give': 'Give',
-#          'auction': 'Auction',
-#          'Assemble': 'Give',
-#          'Mobile%20Device%20Lab': 'MobileDeviceLab',
-#          'kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-#          'Kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-#          'KICKSTARTER': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-#          'key': 'http://signup.hackerdojo.com/key',
-#        }
-#        if pagename in redirect_urls:
-#            url = redirect_urls[pagename]
-#            self.redirect(url, permanent=True)
-#        else:
-#            if CDN_ENABLED:
-#                cdn = CDN_HOSTNAME
-#            try:
-#                pageKey = 'page:%s' % pagename.lower()
-#                if not(pagename):
-#                    pagename = 'FrontPage'
-#                page = _request(PB_API_URL % (site, pagename), cache_ttl=604800, force=skip_cache)
-#                # fetch a page where a lowercase version may exist
-#                if not(page and "name" in page):
-#                  if memcache.get(pageKey):
-#                    pagename = memcache.get(pageKey)
-#                    page = _request(PB_API_URL % (site, pagename), cache_ttl=604800, force=skip_cache)
-#                    shouldRedirect = True
-#                # Convert quasi-camel-case to spaced words
-#                title = re.sub('([a-z]|[A-Z])([A-Z])', r'\1 \2', pagename)
-#                if page and "name" in page:
-#                  fiveDays = 432000
-#                  memcache.set(pageKey, pagename, fiveDays)
-#                  if shouldRedirect:
-#                    self.redirect(pagename, permanent=True)
-#                  else:
-#                    self.response.out.write(template.render('templates/content.html', locals()))
-#                else:
-#                  raise LookupError
-#            except LookupError:
-#                self.response.out.write(template.render('templates/404.html', locals()))
-#                self.response.set_status(404)
+class LogHandler(webapp.RequestHandler):
+    def get(self):
+        event = self.request.get('event')
+        zone = self.request.get('zone')
+        ae = AlarmEvent(event=event, zone=zone)
+        ae.put()
+        self.response.set_status(200)
+
+    def post(self):
+        event = self.request.get('event')
+        zone = self.request.get('zone')
+        ae = AlarmEvent(event=event, zone=zone)
+        ae.put()
+        self.response.set_status(200)
 
 
 
 app = webapp.WSGIApplication([
-#    ('/api/log', LogHandler),
+    ('/api/log', LogHandler),
     ('/', IndexHandler)],
     debug=True)
